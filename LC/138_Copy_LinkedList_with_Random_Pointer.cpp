da4a7bfa55c47cb4d482113d1601e35ea1ae91d8
Saivya <saivya1@gmail.com>
2025-05-26 10:14:06 +0530

    yeah

--- LC/138_Copy_LinkedList_with_Random_Pointer.cpp
+++ LC/138_Copy_LinkedList_with_Random_Pointer.cpp
@@ -1,6 +1,8 @@
 /*A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
 
-Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node.
+ Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state.
+  None of the pointers in the new list should point to nodes in the original list.
 
 For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
 
@@ -13,7 +15,7 @@ The linked list is represented in the input/output as a list of n nodes. Each no
 
 Your code will only be given the head of the original linked list.
 
- 
+
 
 Example 1:
 
@@ -30,27 +32,37 @@ Example 3:
 Input: head = [[3,null],[3,0],[3,null]]
 Output: [[3,null],[3,0],[3,null]]
 */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
-class Solution {
+using namespace std;
+
+class Solution
+{
 public:
-    Node* copyRandomList(Node* head) {
-        if (!head) return nullptr;
-        
-        unordered_map<Node*, Node*> old_to_new;
-        
-        Node* curr = head;
-        while (curr) {
+    Node *copyRandomList(Node *head)
+    {
+        if (!head)
+            return nullptr;
+
+        unordered_map<Node *, Node *> old_to_new;
+
+        Node *curr = head;
+        while (curr)
+        {
             old_to_new[curr] = new Node(curr->val);
             curr = curr->next;
         }
-        
+
         curr = head;
-        while (curr) {
+        while (curr)
+        {
             old_to_new[curr]->next = old_to_new[curr->next];
             old_to_new[curr]->random = old_to_new[curr->random];
             curr = curr->next;
         }
-        
+
         return old_to_new[head];
     }
 };
--- LC/Find_The_Duplicate_Number_LL.cpp
+++ LC/Find_The_Duplicate_Number_LL.cpp
@@ -1,31 +1,58 @@
+/*Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and using only constant extra space.
+
+
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+
+Example 3:
+
+Input: nums = [3,3,3,3,3]
+Output: 3
+*/
 
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include "LinkedList.h"
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findDuplicate(ListNode* nums) {
+    int findDuplicate(ListNode *nums)
+    {
         ListNode *curr = nums;
-        unordered_set<ListNode*> us;
+        unordered_set<ListNode *> us;
 
-        if(!nums){
+        if (!nums)
+        {
             return -1;
         }
-        while(curr){
-            if(us.find(curr) == us.end()){
-            us.insert(curr);
+        while (curr)
+        {
+            if (us.find(curr) == us.end())
+            {
+                us.insert(curr);
             }
-            else{
+            else
+            {
                 return curr->val;
             }
-            curr=curr->next;
-
-
-        }    
-        return -1;  
+            curr = curr->next;
+        }
+        return -1;
     }
 };
