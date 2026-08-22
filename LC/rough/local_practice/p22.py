04a1851452d0704ca66d3b3b6c18de4ab4871a11
Saivya <saivya1@gmail.com>
2026-08-22 16:09:45 +0530

    LC

---
+++ LC/rough/local_practice/p22.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        queue = deque([root])
+        res = []
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+                level.append(node.val)
+            res.append(level[-1])
+
+        return res
