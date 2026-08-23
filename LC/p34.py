a7ba6791c0141b6c050a20df7cdb4ed6b2eecaf1
Saivya <saivya1@gmail.com>
2026-08-23 13:57:51 +0530

    LC

---
+++ LC/p34.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        queue = deque(preorder)
+        um = {}
+
+        for i in range(len(inorder)):
+            um[inorder[i]] = i
+
+        def dfs(left, right):
+            if left > right:
+                return None
+
+            val = queue.popleft()
+            node = TreeNode(val)
+
+            node.left = dfs(left, um[val] - 1)
+            node.right = dfs(um[val] + 1, right)
+
+            return node
+
+        return dfs(0, len(inorder) - 1)
---
+++ LC/rough/local_practice/p24.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        dia = float("-inf")
+
+        def dfs(node):
+            nonlocal dia
+            if node is None:
+                return 0
+
+            left = max(0, dfs(node.left))
+            right = max(0, dfs(node.right))
+
+            dia = max(dia, node.val + left + right)
+
+            return node.val + max(left, right)
+
+        dfs(root)
+
+        return dia
